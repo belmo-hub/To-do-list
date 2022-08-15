be0d9b73fdcc4644dd2c0a56af181da4b158a238
Youssef belmokhtar <belming230@gmail.com>
2022-08-16 00:55:52 +0100

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,68 +1,95 @@
-window.addEventListener('load', function() {
-  const form = document.querySelector('#new-task-form');
-  const input = document.querySelector('#new-task-input');
-  const list_el = document.querySelector("#tasks");
-  
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
+const task_el = document.createElement("div");
+const task_content_el = document.createElement("div");
+const task_input_el = document.createElement("input");
+const task_actions_el = document.createElement("div");
+const task_edit_el = document.createElement("button");
+const task_delete_el = document.createElement("button");
 
-    const task = input.value;
+window.addEventListener("load", function () {
+	const form = document.querySelector("#new-task-form");
+	const input = document.querySelector("#new-task-input");
+	const list_el = document.querySelector("#tasks");
 
-    if(!task) {
-      alert("Please add a task")
-      return;
-    }
-    
-    const task_el = document.createElement("div");
-    task_el.classList.add("task");
-    
-    const task_content_el = document.createElement("div");
-    task_content_el.classList.add("content");
-    // task_content_el.innerText = task;
-    task_el.appendChild(task_content_el);
-    
-    const task_input_el = document.createElement("input");
-    task_input_el.classList.add("text");
-    task_input_el.type = "text";
-    task_input_el.value = task;
-    task_input_el.setAttribute("readonly", "readonly");
+	form.addEventListener("submit", (e) => {
+		e.preventDefault();
 
-    task_content_el.appendChild(task_input_el);
+		const task = input.value;
+		task_input_el.value = task;
 
+		if (!task) {
+			alert("Please add a task");
+			return;
+		}
+		createTask();
 
-    const task_actions_el = document.createElement("div");
-    task_actions_el.classList.add("actions");
-    
-    const task_edit_el = document.createElement("button");
-    task_edit_el.classList.add("edit");
-    task_edit_el.innerHTML = "Edit";
+		list_el.appendChild(task_el);
+		input.value = "";
+	});
+});
 
-    const task_delete_el = document.createElement("button");
-    task_delete_el.classList.add("delete");
-    task_delete_el.innerHTML = "Delete";
+function createTask() {
+	createTask();
+	createTaskContent();
+	createTaskInput();
+	createTaskActions();
+	checkEvent();
+	task_el.classList.add("task");
+}
+function createTaskContent() {
+	task_content_el.classList.add("content");
+	task_el.appendChild(task_content_el);
+	task_actions_el.appendChild(task_edit_el);
+	task_actions_el.appendChild(task_delete_el);
+}
 
-    task_actions_el.appendChild(task_edit_el);
-    task_actions_el.appendChild(task_delete_el);
+function createTaskInput() {
+	task_input_el.classList.add("text");
+	task_input_el.type = "text";
+	task_input_el.setAttribute("readonly", "readonly");
 
-    task_el.appendChild(task_actions_el);
-    list_el.appendChild(task_el);
+	task_content_el.appendChild(task_input_el);
+}
 
-    task_edit_el.addEventListener('click', function(){
-        if (task_edit_el.innerText.toLowerCase() == "edit"){
-          task_input_el.removeAttribute("readOnly");
-          task_input_el.focus();
-          task_edit_el.innerHTML = "Save";
-        } else {
-          task_input_el.setAttribute("readonly", "readonly");
-          task_edit_el.innerText = "Edit";
-        }
-    })
+function createTaskActions() {
+	task_actions_el.classList.add("actions");
+	createEditButton();
+	createDeleteButton();
+	task_el.appendChild(task_actions_el);
+}
 
-    task_delete_el.addEventListener('click', function(){
-      if (task_delete_el.innerText.toLowerCase() == "delete"){
-        task_el.remove();
-      }
-    })
-    input.value = "";
-  })
-})
+function createEditButton() {
+	task_edit_el.classList.add("edit");
+	task_edit_el.innerHTML = "Edit";
+}
+
+function createDeleteButton() {
+	task_delete_el.classList.add("delete");
+	task_delete_el.innerHTML = "Delete";
+}
+
+function editFunction() {
+	if (task_edit_el.innerText.toLowerCase() == "edit") {
+		task_input_el.removeAttribute("readOnly");
+		task_input_el.focus();
+		task_edit_el.innerHTML = "Save";
+	} else {
+		task_input_el.setAttribute("readonly", "readonly");
+		task_edit_el.innerText = "Edit";
+	}
+}
+
+function deleteFunction() {
+	if (task_delete_el.innerText.toLowerCase() == "delete") {
+		task_el.remove();
+	}
+}
+
+function checkEvent() {
+	task_edit_el.addEventListener("click", function () {
+		editFunction();
+	});
+
+	task_delete_el.addEventListener("click", function () {
+		deleteFunction();
+	});
+}
