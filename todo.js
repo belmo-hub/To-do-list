396c15a09c4dce599dc32eecbf2111cc555b6e96
Youssef Belmokhtar <belming230@gmail.com>
2022-08-16 20:06:33 +0100

    Another update

--- todo.js
+++ todo.js
@@ -1,74 +1,87 @@
-const task_el = document.createElement("div");
-const task_content_el = document.createElement("div");
-const task_input_el = document.createElement("input");
-const task_actions_el = document.createElement("div");
-const task_edit_el = document.createElement("button");
-const task_delete_el = document.createElement("button");
-
 window.addEventListener("load", function () {
 	const form = document.querySelector("#new-task-form");
 	const input = document.querySelector("#new-task-input");
-	const list_el = document.querySelector("#tasks");
 
 	form.addEventListener("submit", (e) => {
 		e.preventDefault();
-
 		const task = input.value;
-		task_input_el.value = task;
-
 		if (!task) {
 			alert("Please add a task");
 			return;
 		}
-		createTask();
-
-		list_el.appendChild(task_el);
-		input.value = "";
+		createTask(task);
 	});
 });
 
-function createTask() {
-	task_el.classList.add("task");
-	createTaskContent();
-	createTaskInput();
-	createTaskActions();
-	checkEvent();
-	// createTask();
+function createTask(task) {
+	const list_el = document.querySelector("#tasks");
+	const task_el = document.createElement("div");
+	const input = document.querySelector("#new-task-input");
+	const task_input_el = document.createElement("input");
+	const task_actions_el = document.createElement("div");
+	const task_edit_el = document.createElement("button");
+	const task_delete_el = document.createElement("button");
+	const task_content_el = document.createElement("div");
+
+	task_input_el.value = task;
+	createTaskContent(task_content_el, task_el);
+	createTaskInput(task_content_el, task_input_el);
+	createTaskActions(task_actions_el, task_delete_el, task_edit_el, task_el);
+	checkEvent(task_edit_el, task_delete_el, task_input_el, list_el, task_el);
+	task_el.appendChild(task_actions_el);
+	list_el.appendChild(task_el);
+	input.value = "";
 }
 
-function createTaskContent() {
+function createTaskContent(task_content_el, task_el) {
 	task_content_el.classList.add("content");
-	task_actions_el.appendChild(task_edit_el);
-	task_actions_el.appendChild(task_delete_el);
+	task_el.classList.add("task");
+
 	task_el.appendChild(task_content_el);
 }
 
-function createTaskInput() {
+function createTaskInput(task_content_el, task_input_el) {
 	task_input_el.classList.add("text");
 	task_input_el.type = "text";
 	task_input_el.setAttribute("readonly", "readonly");
-
 	task_content_el.appendChild(task_input_el);
 }
 
-function createTaskActions() {
+function createTaskActions(task_actions_el, task_delete_el, task_edit_el) {
 	task_actions_el.classList.add("actions");
-	createEditButton();
-	createDeleteButton();
-	task_el.appendChild(task_actions_el);
+	createEditButton(task_edit_el);
+	createDeleteButton(task_delete_el);
+	task_actions_el.appendChild(task_edit_el);
+	task_actions_el.appendChild(task_delete_el);
 }
 
-function createEditButton() {
+function createEditButton(task_edit_el) {
 	task_edit_el.classList.add("edit");
 	task_edit_el.innerHTML = "Edit";
 }
 
-function createDeleteButton() {
+function createDeleteButton(task_delete_el) {
 	task_delete_el.classList.add("delete");
 	task_delete_el.innerHTML = "Delete";
 }
 
-function editFunction() {
+function checkEvent(
+	task_edit_el,
+	task_delete_el,
+	task_input_el,
+	list_el,
+	task_el
+) {
+	task_edit_el.addEventListener("click", function () {
+		editFunction(task_edit_el, task_input_el);
+	});
+
+	task_delete_el.addEventListener("click", function () {
+		deleteFunction(task_delete_el, list_el, task_el);
+	});
+}
+
+function editFunction(task_edit_el, task_input_el) {
 	if (task_edit_el.innerText.toLowerCase() == "edit") {
 		task_input_el.removeAttribute("readOnly");
 		task_input_el.focus();
@@ -79,18 +92,8 @@ function editFunction() {
 	}
 }
 
-function deleteFunction() {
+function deleteFunction(task_delete_el, list_el, task_el) {
 	if (task_delete_el.innerText.toLowerCase() == "delete") {
-		task_el.remove();
+		list_el.removeChild(task_el);
 	}
-}
-
-function checkEvent() {
-	task_edit_el.addEventListener("click", function () {
-		editFunction();
-	});
-
-	task_delete_el.addEventListener("click", function () {
-		deleteFunction();
-	});
 }
